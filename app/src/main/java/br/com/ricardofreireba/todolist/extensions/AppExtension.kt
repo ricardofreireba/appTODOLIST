209e33234d1376c1d6e411fe390d5dd3bb4e4333
Ricardo Freire <ricardofreireba@hotmail.com>
2021-09-08 22:15:02 -0300

    app TODOLIST, projeto final Bootcamp Santander

---
+++ app/src/main/java/br/com/ricardofreireba/todolist/extensions/AppExtension.kt
@@ -0,0 +1,18 @@
+package br.com.ricardofreireba.todolist.extensions
+
+import android.icu.util.LocaleData
+import com.google.android.material.textfield.TextInputLayout
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val locale = Locale ("pt", "BR")
+
+fun Date.format() : String {
+    return SimpleDateFormat( "dd/MM/yyyy", locale).format(this)
+}
+
+    var TextInputLayout.text : String
+    get() = editText?.text?.toString() ?:""
+set(value) {
+    editText?.setText(value)
+}
